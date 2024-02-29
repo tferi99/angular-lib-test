fbc49da7d938ace16ae561805d50c78535542b5d
ftoth <toth_ferenc@hotmail.com>
2024-02-29 23:18:34 +0100

    re-organizing tsconfig paths aliases

--- monorepo-client1-and-lib/projects/mono-other-client/src/app/app.module.ts
+++ monorepo-client1-and-lib/projects/mono-other-client/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MyLibModule, ENVIRONMENT_TOKEN } from '@my-lib';
-import { OtherLibDiagnosticsModule } from '@dev/my-other-lib/common';
-import { environment } from '@env/environment.development';
+import { MyLibModule, ENVIRONMENT_TOKEN } from '@my-lib-OTHER';
+import { OtherLibDiagnosticsModule } from '@dev/my-other-lib/common-OTHER';
+import { environment } from '@env-OTHER/environment.development';
 
 @NgModule({
   declarations: [
